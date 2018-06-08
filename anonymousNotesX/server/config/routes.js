4cdc326159acf5acce743d63a546b43e55938b29
Alex Green <alexkenta@gmail.com>
2018-06-07 20:13:22 -0700

    more testing

--- anonymousNotesX/server/config/routes.js
+++
@@ -1,7 +0,0 @@
-const noteController = require('../controllers/notes');
-
-module.exports = function(app){
-    app.get('/notes', noteController.index);
-    
-    app.post('/processNote', noteController.create);
-}
